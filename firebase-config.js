30026d642027f395e91bad09fa170c9408cd815b
Alinsta cell <ayudithea@gmail.com>
2026-08-30 15:42:36 +0700

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyC_PcarMTT5Un7qfSLzf92o5OGn5rginsI",
-  authDomain: "fixzy-update.firebaseapp.com",
-  projectId: "fixzy-update",
-  storageBucket: "fixzy-update.firebasestorage.app",
-  messagingSenderId: "995892519622",
-  appId: "1:995892519622:web:fa41047944d8e5b35d6dc7"
-};
